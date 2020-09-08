02b16d4b3c334da3c32ebaad73599fef3e4e3e67
JoseDanielMX <jduvmx@gmail.com>
2020-09-08 18:48:42 -0500

    Created clock. Deleted text in index.html tags where time and custom messages will appear

--- js/script.js
+++ js/script.js
@@ -1,34 +1,81 @@
 var time = new Date().getHours();
-var image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat5.jpg";
-var messageText;
 var noon = 12;
 var evening = 18;
 var wakeupTime = 9;
 var lunchTime = 12;
 var partyTime = 17;
 var napTime = lunchTime + 2;
-var timeEventJS = document.getElementById("timeEvent");
-var lolcat = document.getElementById("lolcat");
 
-if (time == partyTime){
-	image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat4.jpg";
-    messageText = "IZ PARTEE TIME!!";
-} else if (time == napTime) {
-	image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat3.jpg";
-    messageText = "IZ NAP TIME...";
-} else if (time == lunchTime) {
-	image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat2.jpg";
-    messageText = "IZ NOM NOM NOM TIME!!";
-} else if (time == wakeupTime) {
-	image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat1.jpg";
-    messageText = "IZ TIME TO GETTUP.";
-} else if (time < noon) {
-    messageText = "GOOD MONIN!!";
-} else if (time > evening) {
-    messageText = "GOOD EVENIN!!";
-} else {
-    messageText = "GOOD AFTENOON!!";
-}
+var updateClock = function() {
+    var image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat5.jpg";
+    var messageText;        
+    var timeEventJS = document.getElementById("timeEvent");
+    var lolcat = document.getElementById("lolcat");
 
-timeEventJS.innerText = messageText;
-lolcat.src = image;
+    if (time == partyTime){
+        image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat4.jpg";
+        messageText = "IZ PARTEE TIME!!";
+    } else if (time == napTime) {
+        image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat3.jpg";
+        messageText = "IZ NAP TIME...";
+    } else if (time == lunchTime) {
+        image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat2.jpg";
+        messageText = "IZ NOM NOM NOM TIME!!";
+    } else if (time == wakeupTime) {
+        image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat1.jpg";
+        messageText = "IZ TIME TO GETTUP.";
+    } else if (time < noon) {
+        messageText = "GOOD MONIN!!";
+    } else if (time > evening) {
+        messageText = "GOOD EVENIN!!";
+    } else {
+        messageText = "GOOD AFTENOON!!";
+    }
+
+    timeEventJS.innerText = messageText;
+    lolcat.src = image;
+
+    showCurrentTime();
+
+};
+
+var showCurrentTime = function()
+{
+    // display the string on the webpage
+    var clock = document.getElementById('clock');
+ 
+    var currentTime = new Date();
+ 
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    var meridian = "AM";
+ 
+    // Set hours 
+    if (hours >= noon) { 
+        meridian = "PM"; 
+    }  
+    if (hours > noon) { 
+        hours = hours - 12; 
+    }
+ 
+    // Set Minutes
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+ 
+    // Set Seconds
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+ 
+    // put together the string that displays the time
+    var clockTime = hours + ":" + minutes + ":" + seconds + " " + meridian + "!";
+ 
+    clock.innerText = clockTime;
+
+};
+
+updateClock();
+var oneSecond = 1000;
+setInterval(updateClock, oneSecond);
